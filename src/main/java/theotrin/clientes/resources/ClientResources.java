a6b7c3dd0ce767441f066da4cd62e761f0a1a7fe
unknown <theopinheiro12@gmail.com>
2024-03-11 22:20:51 -0400

    route for add a new client done

--- src/main/java/theotrin/clientes/resources/ClientResources.java
+++ src/main/java/theotrin/clientes/resources/ClientResources.java
@@ -2,12 +2,13 @@ package theotrin.clientes.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import theotrin.clientes.dto.ClientDTO;
 import theotrin.clientes.entities.Client;
 import theotrin.clientes.services.ClientServices;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,8 +19,17 @@ public class ClientResources {
     private ClientServices service;
 
     @GetMapping
-    public ResponseEntity<List<Client>> findAll(){
-        List<Client> clientList = service.findAll();
+    public ResponseEntity<List<ClientDTO>> findAll(){
+        List<ClientDTO> clientList = service.findAll();
         return ResponseEntity.ok().body(clientList);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> create(@RequestBody ClientDTO dto){
+        ClientDTO newClient = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(newClient);
     }
 }
--- src/main/java/theotrin/clientes/services/ClientServices.java
+++ src/main/java/theotrin/clientes/services/ClientServices.java
@@ -2,10 +2,11 @@ package theotrin.clientes.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import theotrin.clientes.dto.ClientDTO;
 import theotrin.clientes.entities.Client;
 import theotrin.clientes.repositories.ClientRepository;
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientServices {
@@ -13,7 +14,28 @@ public class ClientServices {
     @Autowired
     private ClientRepository repository;
 
-    public List<Client> findAll() {
-         return repository.findAll();
+    public List<ClientDTO> findAll() {
+         List<Client> list = repository.findAll();
+
+         List<ClientDTO> listDto = list.stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
+
+         return listDto;
     }
+
+    public ClientDTO insert(ClientDTO data) {
+        Client entity = new Client();
+        copyToEntity(data, entity);
+        repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+
+    private void copyToEntity(ClientDTO dto, Client entity){
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
+
 }
